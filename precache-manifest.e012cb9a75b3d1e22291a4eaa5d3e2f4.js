b17876642aa93fc34aa1e738ea71572161ce0d7d
Aleksandr <deadreebook@gmail.com>
2022-01-19 21:22:18 +0700

    Updates

---
+++ precache-manifest.e012cb9a75b3d1e22291a4eaa5d3e2f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bb7432507c823233e3939fc37d247b",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "d8ec16f243a7560a7da2",
+    "url": "/todolist/static/css/main.f239edcf.chunk.css"
+  },
+  {
+    "revision": "1ce8bd8964bbe26cb2ac",
+    "url": "/todolist/static/js/2.e958e9e9.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.e958e9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ec16f243a7560a7da2",
+    "url": "/todolist/static/js/main.83206cb0.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
